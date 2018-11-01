d66c4f58b0fcf34d698180c1de4b263fcb9c34dd
camarasu.stefan <stefan.camarasu@tremend.com>
2018-11-01 17:12:06 +0200

    submit tmeplate driven form

--- src/app/components/template-form/template-form.component.ts
+++ src/app/components/template-form/template-form.component.ts
@@ -1,23 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+
+export interface IDeserializable {
+  deserialize(props: any): this;
+}
+
+export class Option implements IDeserializable {
+  selected:boolean;
+  id:number|string;
+  title:string;
+  
+  deserialize(props) {
+    Object.assign(this, props);
+    return this;
+  }
+}
+
+const templates = [
+  { id: 1, title: 'url1', selected: false },
+  { id: 2, title: 'url2', selected: false },
+  { id: 3, title: 'url3', selected: false }
+];
 
 @Component({
   selector: 'app-template-form',
   templateUrl: './template-form.component.html',
   styleUrls: ['./template-form.component.scss']
 })
-export class TemplateFormComponent implements OnInit {
+export class TemplateFormComponent implements OnInit, OnChanges {
 
   simpleForm: FormGroup;
+  _options = [];
 
   constructor(private fb: FormBuilder) {
     
   }
 
   ngOnInit() {
-    this.simpleForm = this.fb.group({
-      boxes: ['']
-    })
+    //set collecntions maybe async...
+    this.options = templates;
+
+    // this.simpleForm = this.fb.group({
+    //   options: this.fb.array(this._options)
+    // })
   }
 
+  ngOnChanges(changes:SimpleChanges ) {
+    // update/set collections here
+  }
+
+  set options(options) {
+    this._options = options.map(o => new Option().deserialize(o));
+  }
+
+  get options() {
+    return this._options;
+  }
+
+  onSubmit() {
+    console.log(this.options);
+  }
 }
